5c387cb01c7a2c2fb419c63ed74e62a773158410
Niharikha9 <chniharika.2018@gmail.com>
2020-07-21 14:15:18 +0530

    Installing and running pos

--- RDIP_2/Codes/pos.js
+++ RDIP_2/Codes/pos.js
@@ -100,3 +100,16 @@ function table(){
     document.getElementById('submit').innerHTML="<button style='font-size:20px'>Submit</button>";    
     
 }
+
+    //Notice the space between require and the arguments.
+    
+var pos=require('pos');
+var words = new pos.Lexer().lex('This is some sample text. This text can contain multiple sentences.'); //You have to enter the sentences here
+var tagger = new pos.Tagger();
+var taggedWords = tagger.tag(words);
+for (i in taggedWords) {
+    var taggedWord = taggedWords[i];
+    var word = taggedWord[0];
+    var tag = taggedWord[1];
+    console.log(word + " /" + tag);
+}
